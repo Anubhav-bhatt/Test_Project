cfa1b1b48cba631006b94ad93aa26204faa88666
Anubhav bhatt <81016175+Anubhav-bhatt@users.noreply.github.com>
2024-10-06 12:48:44 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,59 +1,42 @@
 import React from "react";
 
-const products = () => {
+const Products = () => {
   return (
     <div>
-
-
       <div className="text-2xl flex justify-center items-center py-8">
         Products
       </div>
 
-<div className="flex flex-row justify-between space-between">
+      <div className="flex flex-wrap justify-center space-x-4">
+        <div className="mx-4 my-2 border w-80 p-4 rounded-lg shadow-md">
+          <img src="" alt="" />
+          <h1>Card Title</h1>
+          <p>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
 
+        <div className="mx-4 my-2 border w-80 p-4 rounded-lg shadow-md">
+          <img src="" alt="" />
+          <h1>Card Title</h1>
+          <p>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
 
-
-      <div className="mx-auto border w-80 p-4 rounded-lg shadow-md">
-      <img src="" alt=" "  />
-        <h1>Card Title</h1>
-
-        <p>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
+        <div className="mx-4 my-2 border w-80 p-4 rounded-lg shadow-md">
+          <img src="" alt="" />
+          <h1>Card Title</h1>
+          <p>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
       </div>
-
-
-
-      <div className="mx-auto border w-80 p-4 rounded-lg shadow-md">
-        <img src="" alt=" " />
-        <h1>Card Title</h1>
-
-        <p>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
-      </div>
-
-
-
-      <div className="mx-auto border w-80 p-4 rounded-lg shadow-md">
-      <img src="" alt=" " />
-        <h1>Card Title</h1>
-
-        <p>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </p>
-      </div>
-
-
-      </div>
-
-
-
     </div>
   );
 };
 
-export default products;
+export default Products;
